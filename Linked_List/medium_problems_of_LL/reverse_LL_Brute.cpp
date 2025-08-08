369090021182cc7b3f2a23e9365500f307ce9883
sweta <sg8528224@gmail.com>
2025-08-08 16:32:47 +0530

    reverse LL

---
+++ Linked_List/medium_problems_of_LL/reverse_LL_Brute.cpp
@@ -0,0 +1,61 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Node {
+    public :
+    int data;
+    Node * next;
+
+    public :
+    Node(int data1, Node* next1){
+        data=data1;
+        next=next1;
+    }
+
+    public :
+    Node(int data1){
+        data=data1;
+        next=NULL;
+    }
+};
+
+Node * convertarr2LL(vector<int> &arr){
+        Node *head= new Node(arr[0]);
+        Node *mover=head;
+        for(int i=1; i<arr.size();i++){
+            Node* temp=new Node(arr[i]);
+            mover->next=temp;
+            mover=mover->next;
+            
+        }
+        return head;
+    }
+
+Node * reverse_LL(Node * head){
+    Node *temp=head;
+    stack<int> st;
+    while(temp){
+        st.push(temp->data);
+        temp=temp->next;
+    }
+    temp=head;
+    while(temp){
+        temp->data=st.top();
+        st.pop();
+        temp=temp->next;
+    }
+   return head;
+}
+
+
+int main(){
+    vector<int> arr={2,5,8,7};
+    Node * head=convertarr2LL(arr);
+    head=reverse_LL(head);
+    Node *temp=head;
+    while(temp){
+        cout<<temp->data<<" ";
+        temp=temp->next;
+
+    }
+}

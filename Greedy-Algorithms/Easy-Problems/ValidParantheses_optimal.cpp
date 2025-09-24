fd2623ab4a3e7323d7299e720ba826cca6e37cd1
sweta <sg8528224@gmail.com>
2025-09-24 23:12:07 +0530

    new problem

---
+++ Greedy-Algorithms/Easy-Problems/ValidParantheses_optimal.cpp
@@ -0,0 +1,39 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    bool checkValidString(string s) {
+        int mini=0;
+        int maxi=0;
+
+        for(int i=0;i<s.length();i++){
+            if(s[i]=='('){
+                mini=mini+1;
+                maxi=maxi+1;
+                
+            }
+            else if(s[i]==')'){
+                mini=mini-1;
+                maxi=maxi-1;
+            }
+            else{
+                mini=mini-1;
+                maxi=maxi+1;
+            }
+            if(mini<0) mini=0;
+            if(maxi<0) return false;
+            
+
+        }
+        return (mini==0);
+    }
+};
+
+int main(){
+    string s ="((*))";
+    Solution obj;
+    bool ans = obj.checkValidString(s);
+    cout<<ans<<endl;
+    return 0;
+}

ac717881a824d0b741794f119cc5cc87ff684fe8
sweta <sg8528224@gmail.com>
2026-01-19 23:55:39 +0530

    solved new problems today

---
+++ Binary_search/Binary-Search-on-Answers/medianOfTwoSortedArraysBetterApproach.cpp
@@ -0,0 +1,80 @@
+// Median of 2 sorted arrays
+
+// Given two sorted arrays arr1 and arr2 of size m and n respectively, return the median of the two sorted arrays.
+
+// The median is defined as the middle value of a sorted list of numbers. In case the length of the list is even, the median is the average of the two middle elements.
+
+
+// Example 1
+
+// Input: arr1 = [2, 4, 6], arr2 = [1, 3, 5]
+
+// Output: 3.5
+
+// Explanation: The array after merging arr1 and arr2 will be [ 1, 2, 3, 4, 5, 6 ]. As the length of the merged list is even, the median is the average of the two middle elements. Here two medians are 3 and 4. So the median will be the average of 3 and 4, which is 3.5.
+
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution{
+    public:
+    double median(vector<int>&arr1, vector<int>&arr2){
+        int i=0;
+        int j=0;
+        int m = arr1.size();
+        int n = arr2.size();
+        int cnt =0;
+        int el1 =-1;
+        int el2 =-1;
+        int ind1 = (m+n)/2;
+        int ind2 = (m+n-1)/2;
+
+        while(i<m && j<n){
+            if(arr1[i]<arr2[j]){
+                if(ind1==cnt) el1 = arr1[i];
+                if(ind2==cnt) el2 = arr1[i];
+                i++;
+                cnt++;
+           }
+           else{
+            if(ind1==cnt) el1 = arr2[j];
+            if(ind2==cnt) el2 = arr2[j];
+            j++;
+            cnt++;
+           }
+        }
+
+        while(i<m){
+            if(ind1==cnt) el1 = arr1[i];
+            if(ind2==cnt) el2 = arr1[i];
+            i++;
+            cnt++;
+        }
+
+        while (j<n)
+        {
+            if(ind1==cnt) el1 = arr2[j];
+            if(ind2==cnt) el2 = arr2[j];
+            j++;
+            cnt++;
+        }
+
+        if((m+n)%2==1){
+            return el2;
+        }
+        return (double)((double)(el1 +el2)/2.0);
+    }
+};
+
+int main(){
+    Solution sol;
+    vector<int>arr1 = {1, 3};
+    vector<int>arr2 = {2};
+    cout << "Median is: " << sol.median(arr1, arr2) << endl;
+
+    vector<int>arr3 = {1, 2};
+    vector<int>arr4 = {3, 4};
+    cout << "Median is: " << sol.median(arr3, arr4) << endl;
+
+    return 0;
+}
